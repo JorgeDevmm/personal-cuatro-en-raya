69201d3cb214c39db31c38f25ca2e58c7884401d
JorgeDevmm <jorge29luis10@gmail.com>
2024-01-13 13:59:22 -0500

    Agregando función introducir_ficha,ingreso_ficha_usuario

--- juego.py
+++ juego.py
@@ -20,7 +20,7 @@ def mostrar_tablero(tablero):
     """
     # sacamos por pantalla la cabecera, de acuerdo a la longitud de una posición
     for num in range(len(tablero[0])):
-        print(num, end="  ")
+        print(num, end="  ")  # cambio el salto de línea por defecto por espacios
 
     # sacamos por pantalla el tablero
     for fila in tablero:
@@ -29,3 +29,35 @@ def mostrar_tablero(tablero):
         for casilla in fila:
             print(casilla, end="  ")
 
+
+def introducir_ficha(tablero, columna, color):
+    """Esta función introduce una ficha en el tablero indicado.
+
+    :param tablero: Lista.
+    :param columna: Número de columnas.
+    :param color: Representación de la ficha del tablero
+    :return:
+    """
+    # validamos no pasarnos el rango de la longitud de la primera fila (columnas) y menor a 0
+    if columna >= len(tablero[0]) or columna < 0:
+        print(f"Error: Número columna {columna} fuera del rango")
+        return
+    elif tablero[0][columna] != '.':  # válida si la fila y la columna respectiva es diferente a punto
+        print("Error: La columna esta llena de fichas")
+    else:
+        # recorremos nuestro tablero desde abajo hacia arriba para validar si hay elemento e insertar
+        for fila in range(len(tablero) - 1, -1, -1):
+            # válida si existe un punto en tablero significa que esta vacío
+            if tablero[fila][columna] == ".":
+                tablero[fila][columna] = color
+                # retorna el tablero modificado con la nueva ficha
+                return tablero
+
+
+def ingreso_ficha_usuario():
+    """Ingreso de la columna y el tipo de ficha de usuario """
+
+    columna = int(input("Ingresar la posición de columna a ingresar ficha: "))
+    color = input("Ingresar el tipo de ficha a utilizar: ")
+
+    return columna, color
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from juego import crear_tablero, mostrar_tablero
+from juego import crear_tablero, mostrar_tablero,ingreso_ficha_usuario,introducir_ficha
 
 
 def ejecutar_juego():
     tablero_asignado = crear_tablero(6, 7)
+    columna,color = ingreso_ficha_usuario()
+    introducir_ficha(tablero_asignado,columna,color)
     mostrar_tablero(tablero_asignado)
 
 
 if __name__ == '__main__':
     ejecutar_juego()
+    # help(print)
